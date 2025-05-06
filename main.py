2db2b6a4fe454c392acf782aa0e0e5b0407def8b
vitallcore <vitaslipknot@gmail.com>
2025-05-06 18:49:13 +0600

    sadovnik

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def about_page():
     return render_template("about.html")
 
 
+@app.route("/services")
+def services_page():
+    return render_template("services.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
